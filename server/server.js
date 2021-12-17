45950a71cbb8a340f3e278574302628a5140a23a
AnatoliiBass <bass_1304@ukr.net>
2021-12-17 15:13:54 +0200

    continue to make client and server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 require('dotenv').config({ path: './.env' })
 const express = require('express')
 const app = express()
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc')
 const { resolve } = require('path')
 const bodyParser = require('body-parser')
 
 app.use(express.static('../public'))
 app.use(bodyParser.json())
+
+console.log(stripe.checkout);
 
 app.get('/', (req, res) => {
    const path = resolve('../public' + '/index.html')
@@ -14,12 +16,34 @@ app.get('/', (req, res) => {
 })
 
 app.get('/public-keys', (req, res) => {
-   res.send({ key: 'pk_test_51Jw1NSCCeUycvZLCAtXl93HKaxrrvFLbKFp3wT8GWxf7xCIqMYc8n2SEUU5Tpk3beIirwM9KaKvPsAMEg57I6Y4E00xYCu9hyT' })
+   res.send({ key: process.env.STRIPE_PUBLIC_KEY })
 })
 
 app.post('/my-route', (req, res) => {
    console.log('body', req.body)
    res.send(req.body)
 })
+
+app.post('/create-checkout-session', async (req, res) => {
+   const session = await stripe.checkout.sessions.create({
+      line_items: [
+         {
+            price_data: {
+               currency: 'usd',
+               product_data: {
+                  name: 'T-shirt',
+               },
+               unit_amount: 2000,
+            },
+            quantity: 1,
+         },
+      ],
+      mode: 'payment',
+      success_url: 'https://example.com/success',
+      cancel_url: 'https://example.com/cancel',
+   });
+
+   res.redirect(303, session.url);
+});
 
 app.listen(4242, () => console.log('Running on http://localhost:4242'))
